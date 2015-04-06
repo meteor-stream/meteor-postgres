8042e576909629019a1e20f51cdd0833e76d6c63
Eddie Dolan <dolan.charles06@gmail.com>
2015-04-06 16:51:23 -0500

    Updated dependencies, still debugging

--- packages/postgres/pg-live/index2.js
+++ packages/postgres/pg-live/index2.js
@@ -8,7 +8,7 @@ console.log(LiveSQL.LiveSQL); // To show that LiveSQL is avaiable here
 // RIGHT NOW NOTHING FROM THIS FILE IS BEING EXPORTED SO IT IS
 // BASICALLY UNUSABLE
 
-var liveDb = new LiveSQL.LiveSQL(CONN_STR, CHANNEL);
+var liveDb = new LiveSQL(CONN_STR, CHANNEL);
 
 liveDb.select("\n\tSELECT\n\t\tstudents.name  AS student_name,\n\t\tstudents.id    AS student_id,\n\t\tassignments.id AS assignment_id,\n\t\tscores.id      AS score_id,\n\t\tassignments.name,\n\t\tassignments.value,\n\t\tscores.score\n\tFROM\n\t\tscores\n\tINNER JOIN assignments ON\n\t\t(assignments.id = scores.assignment_id)\n\tINNER JOIN students ON\n\t\t(students.id = scores.student_id)\n\tWHERE\n\t\tassignments.class_id = $1\n\tORDER BY\n\t\tscore DESC\n", [1], function (diff, rows) {
 	console.log(util.inspect(diff, { depth: null }), rows);
