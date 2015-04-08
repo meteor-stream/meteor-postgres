d44907f749be614fdc13f8d3b6764366a6235f3f
drceric <drc.ericlau@gmail.com>
2015-04-08 01:11:01 -0400

    dropTable and Create

--- packages/postgres/postgres_tests.js
+++ packages/postgres/postgres_tests.js
@@ -1,10 +1,24 @@
-Tinytest.add("Postgres - basics", function (test) {
+Tinytest.addAsync("Postgres - basics", function (test) {
   // first delete table
-  if (Meteor.isServer) {
-    console.log('test run');
-    Postgres.dropTable('tasks');
-    test.isTrue(true);
-  }
+
   // then create table
+
+  if (Meteor.isServer) {
+    console.log('in test');
+
+    //Drop table work, but, it seems like the notify does not be dropped.
+    Postgres.dropTable('tasks');
+
+    var taskTable = {
+      text: 'varchar (255) not null'
+    };
+    // Create Table does not create the table here, work for the original one
+    Postgres.createTable('tasks', taskTable);
+
+    Postgres.createTable('test', taskTable);
+  }
+
+  test.equal(true, true);
+
 })
 
