5e05783ca33cf729d82a12c1a37dd85870912de0
drceric <drc.ericlau@gmail.com>
2015-04-08 13:47:17 -0400

    comment out the postgres test waiting

--- packages/postgres/postgres-tests.js
+++ packages/postgres/postgres-tests.js
@@ -13,17 +13,23 @@ Tinytest.add("Postgres - basics", function (test) {
 
     // way to detect the existence of specific table?
 
+    // var taskTable = {
+    //   text: [Postgres._DataTypes['$string'], Postgres._TableConstraints['$notnull']]
+    // };
+
     var taskTable = {
-      text: 'varchar (255) not null'
+      text: [Postgres._DataTypes['$string'], 'not null']
     };
     // Create Table does not create the table here, work for the original one
     Postgres.createTable('tasks', taskTable);
 
-    var newTable = {
-      dueDate: 'varchar (255) not null'
-    };
+    // Postgres.createTable('students', { name: [$string, $notnull], age: [$number] });
 
-    Postgres.addColumn('tasks', newTable);
+    // var newTable = {
+    //   dueDate: ['varchar (255)', 'not null']
+    // };
+
+    // Postgres.addColumn('tasks', newTable);
 
 
   }
