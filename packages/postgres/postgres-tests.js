7fa4210e5a6e1e9961d52796c3a5dbd5b88dfa29
drceric <drc.ericlau@gmail.com>
2015-04-08 12:37:13 -0400

    comment

--- packages/postgres/postgres-tests.js
+++ packages/postgres/postgres-tests.js
@@ -1,3 +1,5 @@
+console.log('in test file');
+
 Tinytest.add("Postgres - basics", function (test) {
   // first delete table
 
@@ -9,12 +11,21 @@ Tinytest.add("Postgres - basics", function (test) {
     //Drop table work, but, it seems like the notify does not be dropped.
     Postgres.dropTable('tasks');
 
+    // way to detect the existence of specific table?
+
     var taskTable = {
-      id: ['int', 'not null'],
-      text: ['varchar (255)', 'not null']
+      text: 'varchar (255) not null'
     };
     // Create Table does not create the table here, work for the original one
     Postgres.createTable('tasks', taskTable);
+
+    var newTable = {
+      dueDate: 'varchar (255) not null'
+    };
+
+    Postgres.addColumn('tasks', newTable);
+
+
   }
 
   test.equal(true, true);
