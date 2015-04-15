3eece9735ca13cb1ca1818f43c7c0a963a3e255e
Paulo Diniz <pdiniz_100@yahoo.com>
2015-04-15 14:09:24 -0700

    (fix) fixed the drop table order, and implementation

--- packages/postgres/activerecord.js
+++ packages/postgres/activerecord.js
@@ -375,14 +375,14 @@ ActiveRecord.prototype.createRelationship = function(relTable, relationship, col
     relTable + "_id integer references " + relTable + "(_id)," +
     this.table + relTable + " PRIMARY KEY(" + this.table + "_id, " + relTable + "_id)";
     this.inputString +=  "CREATE OR REPLACE FUNCTION " + this.table + relTable + "(" + columnNames[0][0] + " " +
-    columnNames[0][1] + ", " + columnNames[1][0] + " " + columnNames[1][1] + ") RETURNS trigger AS $$" +
-    " IF (TG_OP = 'DELETE') THEN " +
-    "DELETE FROM " + this.table + relTable + " WHERE " + this.table + relTable + "_id = OLD._id" +
-    "RETURN old;" +
+    columnNames[0][1] + ", " + columnNames[1][0] + " " + columnNames[1][1] + ") RETURNS trigger AS $$ " +
+    "IF (TG_OP = 'DELETE') THEN " +
+    "DELETE FROM " + this.table + relTable + " WHERE " + this.table + relTable + "_id = OLD._id " +
+    "RETURN old; " +
     "ELSIF (TG_OP = 'INSERT') THEN " +
     "INSERT INTO " + this.table + relTable + " ( " + this.table + "_id, " + relTable + "_id, VALUES " +
-    "(NEW._id, (SELECT _id from " + relTable + "WHERE " + relTable  + "_id = value;)" +
-    " RETURN new " +
+    "(NEW._id, (SELECT _id from " + relTable + "WHERE " + relTable  + "_id = value;) " +
+    "RETURN new " +
     "END IF; " +
     "END; " +
     "$$ LANGUAGE plpgsql; ";
