eb1b72bcdce86dbcf67cf3734775a489a0e51fc5
Eddie Dolan <dolan.charles06@gmail.com>
2015-04-16 21:18:20 -0400

    Refactored in preparation for publishing

--- packages/meteor-postgres/package.js
+++ packages/meteor-postgres/package.js
@@ -16,7 +16,7 @@ Npm.depends({
 
 Package.onUse(function(api) {
   // The order these files are imported is very important
-  api.versionsFrom('1.1');
+  api.versionsFrom('1.1.0.1');
   api.use('underscore');
   api.use('tracker');
   api.use('ddp');
