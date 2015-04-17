aa4aaaa91a674fa470f40fd1bad3ebd7e261d762
Eddie Dolan <dolan.charles06@gmail.com>
2015-04-16 21:04:58 -0400

    Refactored repo to be a standalone Meteor Package

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'PostgreSQL support for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/Eddolan/meteor-postgres',
+  git: 'https://github.com/meteor-stream/meteor-postgres',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
