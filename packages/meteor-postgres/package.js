933c0db421b285242dfec68ab5ec8ab5daa5c8e9
Eddie Dolan <dolan.charles06@gmail.com>
2015-04-16 21:35:21 -0400

    Updated package name for publication

--- packages/meteor-postgres/package.js
+++ packages/meteor-postgres/package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'spaceelephant:meteor-postgres',
+  name: 'meteorsteam:meteor-postgres',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'PostgreSQL support for Meteor',
