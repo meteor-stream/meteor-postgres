adb0d60096369549e049c5e2fc16c9b80a53aa65
drceric <drc.ericlau@gmail.com>
2015-04-20 02:02:39 -0400

    (fix) add clear all to first, last and take to override the other commands

--- packages/meteor-postgres/postgres/activerecord.js
+++ packages/meteor-postgres/postgres/activerecord.js
@@ -298,6 +298,7 @@ ActiveRecord.prototype.group = function (group) {
 // QUERY/INPUT STRING
 ActiveRecord.prototype.first = function (limit) {
   limit = limit || 1;
+  this.clearAll();
   this.inputString += 'SELECT * FROM ' + this.table + ' ORDER BY ' + this.table + '.id ASC LIMIT ' + limit + ';';
   this.prevFunc = 'FIRST';
   return this;
@@ -309,6 +310,7 @@ ActiveRecord.prototype.first = function (limit) {
 // QUERY/INPUT STRING
 ActiveRecord.prototype.last = function (limit) {
   limit = limit || 1;
+  this.clearAll();
   this.inputString += 'SELECT * FROM ' + this.table + ' ORDER BY ' + this.table + '.id DESC LIMIT ' + limit + ';';
   this.prevFunc = 'LAST';
   return this;
@@ -320,6 +322,7 @@ ActiveRecord.prototype.last = function (limit) {
 // QUERY/INPUT STRING
 ActiveRecord.prototype.take = function (limit) {
   limit = limit || 1;
+  this.clearAll();
   this.inputString += 'SELECT * FROM ' + this.table + ' LIMIT ' + limit + ';';
   this.prevFunc = 'TAKE';
   return this;
