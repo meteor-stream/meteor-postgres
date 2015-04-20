3272f42b261cdc0cdad10ea663320dd72baef148
Paulo Diniz <pdiniz_100@yahoo.com>
2015-04-20 11:57:22 -0700

    (fix) fixed bug in to dos and where

--- simple-todos.js
+++ simple-todos.js
@@ -3,70 +3,56 @@ tasks = new SQL.Collection('tasks', 'postgres://postgres:1234@localhost/postgres
 users1 = new SQL.Collection('users1', 'postgres://postgres:1234@localhost/postgres');
 
 if (Meteor.isClient) {
-
-  // Creating schema for tables
+  var newUser = 'ko';
   var taskTable = {
     id: ['$number'],
     text: ['$string', '$notnull'],
     checked: ['$bool'],
     users1id: ['$number']
   };
-  // creating table
+
   tasks.createTable(taskTable);
 
-  // creating schema for second table
   var usersTable = {
     id: ['$number'],
     name: ['$string', '$notnull']
   };
-  // creating second table
   users1.createTable(usersTable);
 
 
   Template.body.helpers({
     tasks: function () {
-      // selecting and returning from minisql - client side database
-      var uTasks = tasks.select('tasks.id', 'tasks.text', 'tasks.checked', 'tasks.createdat', 'users1.name')
-                        .join(['LEFT OUTER JOIN'], ['users1id'], [['users1', ['id']]])
-                        .fetch('client');
+      // Also where are the params for the search?
+      var uTasks = tasks.select('tasks.id', 'tasks.text', 'tasks.checked', 'tasks.createdat', 'users1.name').join(['RIGHT OUTER JOIN'], ['users1id'], [['users1', ['id']]]).fetch();
       return uTasks;
     },
     categories: function () {
-      // selecting and returning from minisql
-      return users1.select()
-                   .fetch('client');
+      return users1.select().fetch();
     }
   });
 
   Template.body.events({
     "submit .new-task": function (event) {
-      // TODO: rewrite this to use '?' syntax
-      // building up where string
-      var value = event.target.category.value;
-      var user = users1.select('id')
-                       .where('name = ?', value)
-                       .fetch();
+      var user = users1.select('id').where("name = ?", newUser).fetch();
       user = user[0].id;
       var text = event.target.text.value;
-      // Inserting into localDB. meteor-Postgres will update the server
       tasks.insert({
         text:text,
         checked:false,
         users1id: user
       }).save();
       event.target.text.value = "";
+
+      return false;
     },
     "click .toggle-checked": function () {
-      // Updating local db. meteor-Postgres will udpate the server
-      tasks.update({id: this.id, "checked": !this.checked})
-           .where("id = ?", this.id)
-           .save();
+      tasks.update({id: this.id, "checked": !this.checked}).where("id = ?", this.id).save();
     },
     "click .delete": function () {
-      // Deleting from local db. meteor-Postgres will udpate the server
-      tasks.remove()
-           .where("id = ?", this.id)
-           .save();
+      tasks.remove().where("id = ?", this.id).save();
+    },
+    "change .catselect": function(event){
+      newUser = event.target.value;
     }
   });
 
@@ -74,28 +60,26 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 
-  // Commented out calls to create tables on the server
-  //tasks.createTable({text: ['$string'], checked: ["$bool", {$default: false}]}).save();
-  //users1.createTable({name: ['$string']}).save();
-  //tasks.createRelationship('users1', '$onetomany').save();
+  //tasks.ActiveRecord.createTable({text: ['$string'], checked: ["$bool", {$default: false}]}).save();
+  //users1.ActiveRecord.createTable({name: ['$string']}).save();
+  //tasks.ActiveRecord.createRelationship('users1', '$onetomany').save();
 
-  // Defining the meteor.methods to handle inserting into the serverDB. These will be automatically
-  // called when inserting into the localDB. They need to be named {{collection name}}+'save'
-  // This is a limitation of our implementation and will be fixed in later versions
+
   Meteor.methods({
-    taskssave: function(input, dataArray) {
-      tasks.save(input, dataArray);
-    },
     tasksfetch: function(input, dataArray) {
       tasks.fetch(input, dataArray);
     },
+    taskssave: function(input, dataArray) {
+      tasks.save(input, dataArray);
+    },
     users1save: function(input, dataArray) {
-      users1.save(input, dataArray);
+      users1.fetch(input, dataArray);
     },
     users1fetch: function(input, dataArray) {
-      users1.fetch(input, dataArray);
+      users1.save(input, dataArray);
     }
   });
+
 
   // Publishing the collections
   Meteor.publish('tasks', function () {
