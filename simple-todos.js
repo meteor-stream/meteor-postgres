2deb02000010523f6290bd89ecc03e6fe05c5642
Eddie Dolan <dolan.charles06@gmail.com>
2015-04-20 16:07:12 -0400

    Refactored users1 -> usernames and cleaned up code in simple todos

--- simple-todos.js
+++ simple-todos.js
@@ -1,6 +1,6 @@
 // Defining 2 SQL collections. The additional paramater is the postgres connection string which will only run on the server
 tasks = new SQL.Collection('tasks', 'postgres://postgres:1234@localhost/postgres');
-users1 = new SQL.Collection('users1', 'postgres://postgres:1234@localhost/postgres');
+usernames = new SQL.Collection('usernames', 'postgres://postgres:1234@localhost/postgres');
 
 if (Meteor.isClient) {
   var newUser = 'ko';
@@ -8,49 +8,57 @@ if (Meteor.isClient) {
     id: ['$number'],
     text: ['$string', '$notnull'],
     checked: ['$bool'],
-    users1id: ['$number']
+    usernamesid: ['$number']
   };
 
   tasks.createTable(taskTable);
 
-  var usersTable = {
+  var usernamesTable = {
     id: ['$number'],
     name: ['$string', '$notnull']
   };
-  users1.createTable(usersTable);
+  usernames.createTable(usernamesTable);
 
 
   Template.body.helpers({
     tasks: function () {
-      // Also where are the params for the search?
-      console.log(newUser);
-      var uTasks = tasks.select('tasks.id', 'tasks.text', 'tasks.checked', 'tasks.createdat', 'users1.name').join(['OUTER JOIN'], ['users1id'], [['users1', ['id']]]).where("users1.name = ?", newUser).fetch();
+      var uTasks = tasks.select('tasks.id', 'tasks.text', 'tasks.checked', 'tasks.createdat', 'usernames.name')
+                        .join(['OUTER JOIN'], ['usernamesid'], [['usernames', ['id']]])
+                        .where("usernames.name = ?", newUser)
+                        .fetch();
       return uTasks;
     },
     categories: function () {
-      return users1.select().fetch();
+      return usernames.select()
+                      .fetch();
     }
   });
 
   Template.body.events({
     "submit .new-task": function (event) {
-      var user = users1.select('id').where("name = ?", newUser).fetch();
+      var user = usernames.select('id')
+                          .where("name = ?", newUser)
+                          .fetch();
       user = user[0].id;
       var text = event.target.text.value;
       tasks.insert({
         text:text,
         checked:false,
-        users1id: user
+        usernamesid: user
       }).save();
       event.target.text.value = "";
 
       return false;
     },
     "click .toggle-checked": function () {
-      tasks.update({id: this.id, "checked": !this.checked}).where("id = ?", this.id).save();
+      tasks.update({id: this.id, "checked": !this.checked})
+           .where("id = ?", this.id)
+           .save();
     },
     "click .delete": function () {
-      tasks.remove().where("id = ?", this.id).save();
+      tasks.remove()
+           .where("id = ?", this.id)
+           .save();
     },
     "change .catselect": function(event){
       newUser = event.target.value;
@@ -62,20 +70,20 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 
-  //tasks.ActiveRecord.createTable({text: ['$string'], checked: ["$bool", {$default: false}]}).save();
-  //users1.ActiveRecord.createTable({name: ['$string']}).save();
-  //tasks.ActiveRecord.createRelationship('users1', '$onetomany').save();
+  // tasks.createTable({text: ['$string'], checked: ["$bool", {$default: false}]}).save();
+  // usernames.createTable({name: ['$string']}).save();
+  // tasks.createRelationship('usernames', '$onetomany').save();
 
   // Publishing the collections
   tasks.publish('tasks', function(){
-    return tasks.select('tasks.id as id', 'tasks.text', 'tasks.checked', 'tasks.createdat', 'users1.id as users1id', 'users1.name')
-       .join(['INNER JOIN'], ["users1id"], [["users1", 'id']])
+    return tasks.select('tasks.id as id', 'tasks.text', 'tasks.checked', 'tasks.createdat', 'usernames.id as usernamesid', 'usernames.name')
+       .join(['INNER JOIN'], ["usernamesid"], [["usernames", 'id']])
        .order('createdat DESC')
        .limit(100)
   });
 
-  users1.publish('users1', function(){
-    return users1.select('id', 'name')
-                 .limit(100)
+  usernames.publish('usernames', function(){
+    return usernames.select('id', 'name')
+                 .limit(10)
   });
 }
