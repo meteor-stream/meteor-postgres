b8c91a930b66538db745ea253ad841550497eb83
Paulo Diniz <pdiniz_100@yahoo.com>
2015-04-23 13:35:40 -0700

    (fix) changed from by to -

--- simple-todos.js
+++ simple-todos.js
@@ -1,6 +1,6 @@
 // Defining 2 SQL collections. The additional paramater is the postgres connection string which will only run on the server
-tasks = new SQL.Collection('tasks', 'postgres://postgres:1234@localhost/postgres');
-username = new SQL.Collection('username', 'postgres://postgres:1234@localhost/postgres');
+tasks = new SQL.Collection('tasks', 'postgres://meteor:Meteor1234@localhost/meteor');
+username = new SQL.Collection('username', 'postgres://meteor:Meteor1234@localhost/meteor');
 
 if (Meteor.isClient) {
   var newUser = 'all';
