bb7ed00f825ca34401e1e7eb85875ec7bb08f090
barbbella <barbedbella@gmail.com>
2015-04-23 12:59:33 -0500

    (feat) added throw errors if arrays are empty in WHERE..IN statements client & server

--- packages/meteor-postgres/minisql/minisql.js
+++ packages/meteor-postgres/minisql/minisql.js
@@ -214,6 +214,9 @@ miniSQL.prototype.where = function(/*Arguments*/) {
   // replace ? with rest of array
   for (var i = 1, count = arguments.length; i < count; i++) {
     if (Array.isArray(arguments[i])) {
+      if (arguments[i].length === 0) {
+        throw 'Invalid input: array is empty'
+      }
       redux = where.indexOf('?');
       substring1 = where.substring(0, redux);
       substring2 = where.substring(redux + 1, where.length);
